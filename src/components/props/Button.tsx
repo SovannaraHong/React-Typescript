590ab690d762aefa76811289d46d83b72eace8ef
Hong Sovannara <rapewpew372@gmail.com>
2025-11-01 21:53:27 +0700

    counting fun typescript

--- src/components/props/Button.tsx
+++ src/components/props/Button.tsx
@@ -1,5 +1,22 @@
-const Button = () => {
-  return <div>Button</div>;
+import { useContext } from "react";
+import { FunCount } from "./MainProp";
+
+interface ButtonType {
+  text: string;
+}
+
+const Button = ({ text }: ButtonType) => {
+  const { Counting } = useContext(FunCount);
+  return (
+    <>
+      <button
+        onClick={Counting}
+        className=" bg-pink-300 text-white px-[10px] py-[5px] rounded-[4px]"
+      >
+        {text}
+      </button>
+    </>
+  );
 };
 
 export default Button;
--- src/components/props/MainProp.tsx
+++ src/components/props/MainProp.tsx
@@ -1,5 +1,30 @@
+import { createContext, useState } from "react";
+import Button from "./Button";
+interface ButtonType {
+  Counting: () => void;
+}
+export const FunCount = createContext<ButtonType>({
+  Counting: () => {},
+});
 const MainProp = () => {
-  return <div>MainProp</div>;
+  const [count, setCount] = useState<number>(0);
+  function Counting(): void {
+    setCount((prev) => prev + 1);
+  }
+  return (
+    <>
+      <FunCount.Provider value={{ Counting }}>
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex justify-center items-center flex-col gap-3">
+            <h1 className="bg-green-300 text-white font-monospace font-semibold rounded-[5px]  px-[20px] py-[5px]">
+              Count: {count}
+            </h1>
+            <Button text="click me😍" />
+          </div>
+        </div>
+      </FunCount.Provider>
+    </>
+  );
 };
 
 export default MainProp;
